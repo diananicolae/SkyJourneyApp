17c131ed379535577f2e48a0b742341d2e61e4f3
Diana Nicolae <diana.nicolae19@yahoo.com>
2024-01-08 11:06:42 +0200

    Modify Docker build script

--- update_docker_images.sh
+++ update_docker_images.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+cd sky-journey-db && ./gradlew clean bootJar
+docker buildx build --platform linux/amd64,linux/arm64 -t diananicolae/sky-journey-db:latest --push . && cd ..
+
 cd sky-journey-core && ./gradlew clean bootJar
-docker build -t diananicolae/sky-journey-core:latest . && cd ..
-docker push diananicolae/sky-journey-core:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t diananicolae/sky-journey-core:latest --push . && cd ..
 
 cd sky-journey-payment && ./gradlew clean bootJar
-docker build -t diananicolae/sky-journey-payment:latest . && cd ..
-docker push diananicolae/sky-journey-payment:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t diananicolae/sky-journey-payment:latest --push . && cd ..
 
 cd sky-journey-auth && ./gradlew clean bootJar
-docker build -t diananicolae/sky-journey-auth:latest . && cd ..
-docker push diananicolae/sky-journey-auth:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t diananicolae/sky-journey-auth:latest --push . && cd ..
