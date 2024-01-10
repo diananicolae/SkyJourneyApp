a131b126cb786278d3abd6ec3bbd7e3e6e975ac6
Diana Nicolae <diana.nicolae19@yahoo.com>
2024-01-10 21:57:39 +0200

    Add cluster config

--- apply_kube_changes.sh
+++ apply_kube_changes.sh
@@ -14,3 +14,14 @@ kubectl apply -f sky-journey-auth/sky-journey-auth-service.yaml
 
 kubectl apply -f sky-journey-ui/sky-journey-ui-deployment.yaml
 kubectl apply -f sky-journey-ui/sky-journey-ui-service.yaml
+
+kubectl apply -f sky-journey-dbadmin/sky-journey-dbadmin-deployment.yaml
+kubectl apply -f sky-journey-dbadmin/sky-journey-dbadmin-service.yaml
+
+kubectl apply -f sky-journey-portainer/portainer-sa.yaml
+kubectl apply -f sky-journey-portainer/portainer-cluster.yaml
+kubectl apply -f sky-journey-portainer/portainer-rbac.yaml
+kubectl apply -f sky-journey-portainer/sky-journey-portainer-deployment.yaml
+kubectl apply -f sky-journey-portainer/sky-journey-portainer-service.yaml
+kubectl apply -f https://downloads.portainer.io/ce2-19/portainer-agent-k8s-nodeport.yaml
+
